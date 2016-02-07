c57765ef55b175957b5627d40a398dd2a725488b
Jennifer Pacheco <jvivian14@hotmail.com>
2016-02-07 16:24:22 -0500

    se agrego quorum actualizado constantemente

--- apppachecograficas/Quorum.Designer.cs
+++ apppachecograficas/Quorum.Designer.cs
@@ -54,11 +54,13 @@
             this.label1.Size = new System.Drawing.Size(99, 33);
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
             this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Quorum
             // 
--- apppachecograficas/Quorum.cs
+++ apppachecograficas/Quorum.cs
@@ -25,31 +25,66 @@ namespace apppachecograficas
         private void Quorum_Load(object sender, EventArgs e)
         {
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.cargarQuorum();
 
         }
         private void cargarQuorum()
         {
-            string quorum = "";
+            //string quorum = "";
+            //ConexionPostgres conn = new ConexionPostgres();
+
+            //var cadenaSql = "SELECT sum(b.coeficiente) FROM modelo.asamblea_unidad_residencial AS a LEFT JOIN modelo.unidad_residencial AS b ON (a.numero_unidad = b.numero_unidad AND a.nit = b.nit) WHERE a.nit='"+this.nit+"' AND a.fecha='"+this.fecha+"';";
+            //double asistenciaCasoCoeficientes = Double.Parse(conn.consultar(cadenaSql)[0]["sum"]);
+
+            //cadenaSql = "SELECT sum(coeficiente) FROM modelo.unidad_residencial WHERE nit='"+this.nit+"';";
+            //double registradosCasoCoeficientes = Double.Parse(conn.consultar(cadenaSql)[0]["sum"]);
+
+            //cadenaSql = "SELECT sum(b.coeficiente) FROM modelo.asamblea_unidad_residencial AS a LEFT JOIN modelo.unidad_residencial AS b ON (a.numero_unidad = b.numero_unidad AND a.nit = b.nit) WHERE a.nit='" + this.nit + "' AND a.fecha='" + this.fecha + "' and id_tipo_asistencia_final ='4';";
+            //double registradosCasoUnidadesdescargue = Double.Parse(conn.consultar(cadenaSql)[0]["sum"]);
+
+            //double porcentaje = (100 * (asistenciaCasoCoeficientes - registradosCasoUnidadesdescargue) / registradosCasoCoeficientes);
+            //porcentaje = Math.Round(porcentaje, 2);
+            //quorum = (porcentaje).ToString();
+
             ConexionPostgres conn = new ConexionPostgres();
-
-            var cadenaSql = "SELECT sum(b.coeficiente) FROM modelo.asamblea_unidad_residencial AS a LEFT JOIN modelo.unidad_residencial AS b ON (a.numero_unidad = b.numero_unidad AND a.nit = b.nit) WHERE a.nit='"+this.nit+"' AND a.fecha='"+this.fecha+"';";
+            string quorum = "";
+            var cadenaSql = "SELECT sum(b.coeficiente) FROM modelo.asamblea_unidad_residencial AS a LEFT JOIN modelo.unidad_residencial AS b ON (a.numero_unidad = b.numero_unidad AND a.nit = b.nit) WHERE a.nit='" + this.nit + "' AND a.fecha='" + this.fecha + "';";
             double asistenciaCasoCoeficientes = Double.Parse(conn.consultar(cadenaSql)[0]["sum"]);
-
-            cadenaSql = "SELECT sum(coeficiente) FROM modelo.unidad_residencial WHERE nit='"+this.nit+"';";
+            cadenaSql = "SELECT sum(coeficiente) FROM modelo.unidad_residencial WHERE nit='" + this.nit + "';";
             double registradosCasoCoeficientes = Double.Parse(conn.consultar(cadenaSql)[0]["sum"]);
-
-            cadenaSql = "SELECT sum(b.coeficiente) FROM modelo.asamblea_unidad_residencial AS a LEFT JOIN modelo.unidad_residencial AS b ON (a.numero_unidad = b.numero_unidad AND a.nit = b.nit) WHERE a.nit='" + this.nit + "' AND a.fecha='" + this.fecha + "' and id_tipo_asistencia_final ='4';";
-            double registradosCasoUnidadesdescargue = Double.Parse(conn.consultar(cadenaSql)[0]["sum"]);
-
-            double porcentaje = (100 * (asistenciaCasoCoeficientes - registradosCasoUnidadesdescargue) / registradosCasoCoeficientes);
-            porcentaje = Math.Round(porcentaje, 2);
-            quorum = (porcentaje).ToString();
-
-            label1.Text = quorum + "%";
-            if (porcentaje >= 51)
+            var cadenaSql1 = "SELECT sum(b.coeficiente) FROM modelo.asamblea_unidad_residencial AS a LEFT JOIN modelo.unidad_residencial AS b ON (a.numero_unidad = b.numero_unidad AND a.nit = b.nit) WHERE a.nit='" + this.nit + "' AND a.fecha='" + this.fecha + "' and id_tipo_asistencia_final ='4';";
+            var registro = conn.consultar(cadenaSql1);
+            if (registro[0]["sum"] != "")
             {
-                label1.ForeColor = System.Drawing.Color.Green;
+                double registradosCasoUnidadesdescargue = Double.Parse(conn.consultar(cadenaSql1)[0]["sum"]);
+                double porcentaje1 = (100 * (asistenciaCasoCoeficientes - registradosCasoUnidadesdescargue) / registradosCasoCoeficientes);
+                porcentaje1 = Math.Round(porcentaje1, 2);
+                quorum = (porcentaje1).ToString();
+                label1.Text = quorum + "%";
             }
+            else
+            {
+                double porcentaje = (100 * (asistenciaCasoCoeficientes) / registradosCasoCoeficientes);
+                porcentaje = Math.Round(porcentaje, 2);
+                quorum = (porcentaje).ToString();
+                label1.Text = quorum + "%";
+            }
+
+
+            //if (quorum >= 51)
+            //{
+            //    label1.ForeColor = System.Drawing.Color.Green;
+            //}
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.cargarQuorum();
         }
     }
 }
